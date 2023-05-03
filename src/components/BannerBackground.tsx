3eedeca92b044cb056de8eb3725cdc8fc0a695e4
AppLab <silver.designer.developer@gmail.com>
2023-05-03 13:19:02 +0200

    update

---
+++ src/components/BannerBackground.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Banner from '../assets/images/background.jpeg';
+const BannerBackground = () => {
+    return (
+        <Box
+        sx={{
+          height: '50vh',
+          backgroundImage: `url(${Banner})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          '& h1': {
+            fontSize: '4rem',
+            fontWeight: 700,
+            color: 'black',
+            backgroundColor: 'rgba(255,255,255,0.7)',
+            borderRadius: '10px',
+            padding: '20px',
+            backdropFilter: 'blur(10px)',
+            boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.3)',
+          },
+        }}
+      >
+        <h1>Genera il codice commessa</h1>
+      </Box>
+      );
+  };
+  
+  export default BannerBackground;
--- src/components/GenerateUniqueId.tsx
+++ src/components/GenerateUniqueId.tsx
@@ -21,6 +21,7 @@ import {
   SearchOutlined,
 } from "@mui/icons-material";
 import Header from "./Header";
+import BannerBackground from "./BannerBackground";
 
 interface Article {
   name: string;
@@ -28,23 +29,61 @@ interface Article {
 }
 
 interface Options {
-  productType: boolean;
+  hardwareUsedA: boolean;
+  hardwareUsedB: boolean;
+  hardwareUsedC: boolean;
+  hardwareUsedD: boolean;
+  hardwareUsedE: boolean;
+  hardwareUsedF: boolean;
+  hardwareUsedG: boolean;
+  hardwareUsedH: boolean;
+  hardwareUsedI: boolean;
+  hardwareUsedL: boolean;
+  hardwareUsedM: boolean;
+  hardwareUsedN: boolean;
+  hardwareUsedO: boolean;
+  hardwareUsedP: boolean;
+  categorySoftwareA: boolean;
+  categorySoftwareB: boolean;
+  categorySoftwareC: boolean;
   brand: boolean;
   model: boolean;
   serialNumber: boolean;
+  versionProject: boolean;
+  codeCustomer: boolean;
 }
 
 function GenerateUniqueId(): JSX.Element {
   const [options, setOptions] = useState<Options>({
-    productType: false,
+    hardwareUsedA: false,
+    hardwareUsedB: false,
+    hardwareUsedC: false,
+    hardwareUsedD: false,
+    hardwareUsedE: false,
+    hardwareUsedF: false,
+    hardwareUsedG: false,
+    hardwareUsedH: false,
+    hardwareUsedI: false,
+    hardwareUsedL: false,
+    hardwareUsedM: false,
+    hardwareUsedN: false,
+    hardwareUsedO: false,
+    hardwareUsedP: false,
+    categorySoftwareA: false,
+    categorySoftwareB: false,
+    categorySoftwareC: false,
     brand: false,
     model: false,
     serialNumber: false,
+    versionProject: false,
+    codeCustomer: false,
   });
   const [articles, setArticles] = useState<Article[]>([]);
   const [idHistory, setIdHistory] = useState<string[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [serialNumber, setSerialNumber] = useState<number>(1);
+  const [versionProject, setVersionProject] = useState<number>(1);
+  const [codeCustomer, setCodeCustomer] = useState<number>(1);
 
   function handleOptionChange(option: keyof Options) {
     setOptions({
@@ -55,7 +94,26 @@ function GenerateUniqueId(): JSX.Element {
 
   function generateUniqueId() {
     let uniqueId = "";
-    if (options.productType) uniqueId += "TP";
+    if (options.versionProject) uniqueId += `A0${versionProject}`;
+    if (options.codeCustomer) uniqueId += `${codeCustomer}`;
+    if (options.hardwareUsedA) uniqueId += "HA";
+    if (options.hardwareUsedB) uniqueId += "HB";
+    if (options.hardwareUsedC) uniqueId += "HC";
+    if (options.hardwareUsedD) uniqueId += "HD";
+    if (options.hardwareUsedE) uniqueId += "HE";
+    if (options.hardwareUsedF) uniqueId += "HF";
+    if (options.hardwareUsedG) uniqueId += "HG";
+    if (options.hardwareUsedH) uniqueId += "HH";
+    if (options.hardwareUsedI) uniqueId += "HI";
+    if (options.hardwareUsedL) uniqueId += "HL";
+    if (options.hardwareUsedM) uniqueId += "HM";
+    if (options.hardwareUsedN) uniqueId += "HN";
+    if (options.hardwareUsedO) uniqueId += "HO";
+    if (options.hardwareUsedP) uniqueId += "HP";
+    if (options.categorySoftwareA) uniqueId += "CSA";
+    if (options.categorySoftwareB) uniqueId += "CSB";
+    if (options.categorySoftwareC) uniqueId += "CSC";
+
     if (options.brand) uniqueId += "BR";
     if (options.model) uniqueId += "MD";
     if (options.serialNumber) uniqueId += `SN${serialNumber}`;
@@ -70,6 +128,8 @@ function GenerateUniqueId(): JSX.Element {
     setArticles([...articles, newArticle]);
     setIdHistory([...idHistory, newArticle.uniqueId]);
     setSerialNumber(serialNumber + 1);
+    setVersionProject(versionProject + 1);
+    setCodeCustomer(codeCustomer + 1);
     const newIdHistory = [...idHistory, newArticle.uniqueId];
     setIdHistory(newIdHistory);
     // Salva gli ID storici nel localStorage
@@ -103,6 +163,18 @@ function GenerateUniqueId(): JSX.Element {
     setSerialNumber(Number(event.target.value));
   }
 
+  function handleVersionProjectChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setVersionProject(Number(event.target.value));
+  }
+
+  function handleCodeCustomerChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setCodeCustomer(Number(event.target.value));
+  }
+
   const filteredArticles = articles.filter(
     (article) =>
       article.uniqueId.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -130,33 +202,119 @@ function GenerateUniqueId(): JSX.Element {
           GTS - Generatore ID Articoli
         </Typography>
       </Box> */}
-      <Header title=""/>
-      <Container sx={{ py: 8 }}>
-        <Typography variant="h3" align="center">
-          Genera il codice commessa 
-        </Typography>
-        <Box sx={{ mt: 10 }}>
+      <Header title="" />
+      <BannerBackground />
+      <Container sx={{ py: 2 }}>
+        <Box sx={{ mt: 5 }}>
           <Grid container spacing={2} style={{ justifyContent: "center" }}>
             <Grid>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={options.productType}
-                    onChange={() => handleOptionChange("productType")}
+              <div>
+                <h3>Versione del progetto indicato a cliente</h3>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.versionProject}
+                      onChange={() => handleOptionChange("versionProject")}
+                    />
+                  }
+                  label="Inserisci Versione Progetto"
+                />
+                {options.versionProject && (
+                  <TextField
+                    label="Inizio versione progetto"
+                    type="number"
+                    value={versionProject}
+                    onChange={handleVersionProjectChange}
+                    fullWidth
                   />
-                }
-                label="Tipo di prodotto"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={options.brand}
-                    onChange={() => handleOptionChange("brand")}
+                )}
+              </div>
+              <div>
+                <h3>Codice Cliente</h3>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.codeCustomer}
+                      onChange={() => handleOptionChange("codeCustomer")}
+                    />
+                  }
+                  label="Inserisci Codice Cliente"
+                />
+                {options.codeCustomer && (
+                  <TextField
+                    label="Inizio codice cliente"
+                    type="number"
+                    value={codeCustomer}
+                    onChange={handleCodeCustomerChange}
+                    fullWidth
                   />
-                }
-                label="Marca"
-              />
-              <FormControlLabel
+                )}
+              </div>
+              <div>
+                <h3>Seleziona il Tipo di hardware utilizzato</h3>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.hardwareUsedA}
+                      onChange={() => handleOptionChange("hardwareUsedA")}
+                    />
+                  }
+                  label="Bilancia intelligente"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.hardwareUsedB}
+                      onChange={() => handleOptionChange("hardwareUsedB")}
+                    />
+                  }
+                  label="Processore Verticale"
+                />
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.hardwareUsedC}
+                      onChange={() => handleOptionChange("hardwareUsedC")}
+                    />
+                  }
+                  label="PressLog"
+                />
+              </div>
+
+              <div>
+                <h3>Seleziona la categoria del software impiegato</h3>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.categorySoftwareA}
+                      onChange={() => handleOptionChange("categorySoftwareA")}
+                    />
+                  }
+                  label="Sviluppo Web Based"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.categorySoftwareB}
+                      onChange={() => handleOptionChange("categorySoftwareB")}
+                    />
+                  }
+                  label="Firmware ESP32"
+                />
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={options.categorySoftwareC}
+                      onChange={() => handleOptionChange("categorySoftwareC")}
+                    />
+                  }
+                  label="Firmware STM32"
+                />
+              </div>
+
+              {/* <FormControlLabel
                 control={
                   <Checkbox
                     checked={options.model}
@@ -164,8 +322,8 @@ function GenerateUniqueId(): JSX.Element {
                   />
                 }
                 label="Modello"
-              />
-              <FormControlLabel
+              /> */}
+              {/* <FormControlLabel
                 control={
                   <Checkbox
                     checked={options.serialNumber}
@@ -182,21 +340,20 @@ function GenerateUniqueId(): JSX.Element {
                   onChange={handleSerialNumberChange}
                   fullWidth
                 />
-              )}
+              )} */}
             </Grid>
           </Grid>
         </Box>
 
         <Box
           sx={{ mt: 4 }}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
+          style={{ display: "flex", justifyContent: "center" }}>
           <Button variant="contained" startIcon={<Add />} onClick={addArticle}>
             Aggiungi ID Commessa
           </Button>
         </Box>
         <Typography variant="h6" align="left" sx={{ mt: 10 }} id="search">
-          Cerca gli id commesse generate oggi
+          Cerca le commesse generate oggi
         </Typography>
         <Box sx={{ mt: 4 }}>
           <TextField
