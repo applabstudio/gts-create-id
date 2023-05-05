337f87de260d182cf8ebdabb66a59e90f28daae3
AppLab <silver.designer.developer@gmail.com>
2023-05-05 18:54:21 +0200

    seo

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,19 @@ import HomePage from "./pages/HomePage";
 import HistoryPage from "./pages/HistoryPage";
 import "./styles/App.css";
 import CompanyPage from "./pages/CompanyPage";
+import { Helmet } from 'react-helmet';
 // import BannerBackground from "./components/BannerBackground";
 
 const App: FC = () => {
   return (
     <Router>
+      <Helmet>
+        <title>Generatore commesse GTS</title>
+        <meta name="description" content="Web App per commesse GTS" />
+        <meta property="og:title" content="Generatore commesse GTS" />
+        <meta property="og:description" content="La descrizione della tua pagina" />
+        <meta property="og:image" content="public/images/generatorecommesse.png" />
+      </Helmet>
       <Header title="" />
       {/* <BannerBackground/> */}
       <Routes>
